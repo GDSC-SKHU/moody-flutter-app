920cf9d8f8e1b646ae62a2c89914957f350349ac
yujinkim1 <yujin7621@me.com>
2023-01-27 22:18:38 +0900

    add: Palette

---
+++ lib/utilities/palette.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  //MARK: COLOR SET
+  static const white = Color(0xffFFFFFF);
+  static const lightGrey = Color(0xffEDEDED);
+  static const darkGrey = Color(0xff858585);
+
+  //MARK: LIGHT MODE HEATMAP COLOR SET
+  static const lightmaps = {
+    1: Color.fromARGB(120, 255, 116, 115),
+    2: Color.fromARGB(180, 255, 116, 115),
+    3: Color.fromARGB(255, 255, 116, 115)
+  };
+
+  //MARK: DARK MODE HEATMAP COLOR SET
+  static const darktmaps = {
+    1: Color.fromARGB(120, 2, 179, 8),
+    2: Color.fromARGB(180, 2, 179, 8),
+    3: Color.fromARGB(255, 2, 179, 8)
+  };
+}
